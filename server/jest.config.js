1ddf72b6509b17c674536cf37f38f668b82a3451
AbhiAmbhore0905 <abhijeetambhore4@gmail.com>
2026-08-20 13:09:46 +0530

    chore: remove all test files and testing infrastructure

--- server/jest.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  testEnvironment: 'node',
-  transform: {},
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  clearMocks: true,
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
-};
